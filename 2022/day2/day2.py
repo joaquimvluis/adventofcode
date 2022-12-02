0261f1288da500db6439b1279ad9377f2815ff60
joaquimvluis <102586017+joaquimvluis@users.noreply.github.com>
2022-12-02 12:08:17 +0000

    Solved 2nd part of challenge

--- 2022/day2/day2.py
+++ 2022/day2/day2.py
@@ -1,50 +1,52 @@
 shape_score = {'A': 1, 'B': 2, 'C': 3, 'X': 1, 'Y': 2, 'Z': 3}
 outcome_score = {'win': 6, 'draw': 3, 'loss': 0}
-WIN = 6
-DRAW = 3
-LOSS = 0
+shape = { 'A': 'rock', 'X': 'rock', 'B': 'paper', 'Y': 'paper', 'C': 'scissor', 'Z': 'scissor'}
 
 score = 0
 with open('input.txt') as f:
     i = 1
     for line in f.readlines():
         round_score = 0
-        player1 = line[0]
-        player2 = line[2]
+        opponent = line[0]
+        you = line[2]
 
-        if player1 == 'A':
-            if player2 == 'X':
-                print('draw')
-                outcome = 'draw'
-            elif player2 == 'Y':
-                print('player2 wins')
+        if opponent == 'A':
+            if you == 'X':
                 outcome = 'loss'
-            else:
-                print('you win!')
-                outcome = 'win'
-        if player1 == 'B':
-            if player2 == 'Y':
-                print('draw')
+                you = 'C'
+            elif you == 'Y':
                 outcome = 'draw'
-            elif player2 == 'Z':
-                print('player2 wins')
-                outcome = 'loss'
-            else:
-                print('you win!')
+                you = 'A'
+            elif you == 'Z':
                 outcome = 'win'
-        if player1 == 'C':
-            if player2 == 'Z':
-                print('draw')
+                you = 'B'
+        if opponent == 'B':
+            if you == 'X':
+                outcome = 'loss'
+                you = 'A'
+            elif you == 'Y':
                 outcome = 'draw'
-            elif player2 == 'X':
-                print('player2 wins')
-                outcome = 'loss'
-            else:
-                print('you win!')
+                you = 'B'
+            elif you == 'Z':
                 outcome = 'win'
-        round_score = shape_score[player1] + outcome_score[outcome]
+                you = 'C'
+        if opponent == 'C':
+            if you == 'X':
+                outcome = 'loss'
+                you = 'B'
+            elif you == 'Y':
+                outcome = 'draw'
+                you = 'C'
+            elif you == 'Z':
+                outcome = 'win'
+                you = 'A'
+        round_score = shape_score[you] + outcome_score[outcome]
         score += round_score
 
-        print('round {} => score: {} total score:
-                {}'.format(i,round_score,score))
+        # print("round {}: ".format(i))
+        # print("elf played {}, you played {}".format(shape[opponent], shape[you]))
+        # print("shape points: {} outcomepoints: {}".format(shape_score[you], outcome_score[outcome]))
+        # print("score: {} total score: {}\n".format(round_score,score))
 
+        # i += 1
+print("total score {}".format(score))
